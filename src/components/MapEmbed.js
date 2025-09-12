f90d2c82213df5fb59eb90a15bde3918639f39f7
towheeb1 <tow774047284@gmail.com>
2025-09-12 23:33:00 +0300

    تعديل mapings

--- src/components/MapEmbed.js
+++ src/components/MapEmbed.js
@@ -1,19 +1,17 @@
-// src/components/MapEmbed.jsx
-import React from 'react';
-import { FaMapMarkerAlt, FaCalendarAlt, FaExternalLinkAlt } from 'react-icons/fa'; // استيراد الأيقونات
+ import React from 'react';
+import { FaMapMarkerAlt, FaCalendarAlt, FaExternalLinkAlt } from 'react-icons/fa';  
 import '../styles/MapEmbed.css';
 
 const MapEmbed = () => {
   const campusInfo = {
     name: "Malmö University",
-    address: "Universitetsgatan 10, 211 19 Malmö, Sweden", // تحديث العنوان
+    address: "Universitetsgatan 10, 211 19 Malmö, Sweden",  
     officeHours: [
       "Monday - Friday: 9:00 AM - 5:00 PM",
       "Saturday: 9:00 AM - 1:00 PM",
       "Sunday: Closed"
     ],
-    // تحديث رابط الخريطة لجامعة Malmö
-    mapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2914.510256315163!2d44.17173926337391!3d15.359849803621387!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1603ddd3aa42ec8f%3A0x945d25ab2494c6d!2z2KzYp9mF2LnYqSDYp9mE2YbYp9i12LEg2KfZhNin2K_Yp9ix2Kkg2KfZhNi52KfZhdip!5e0!3m2!1sar!2s!4v1757706191316!5m2!1sar!2s"
+     mapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2220.098291517993!2d13.000160377544966!3d55.60037099209096!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4653a52e8451c7d7%3A0x4019078290e7c40!2sMalm%C3%B6%20University!5e0!3m2!1sen!2sse!4v1718574820277!5m2!1sen!2sse"
   };
 
   return (
@@ -23,7 +21,7 @@ const MapEmbed = () => {
         <div className="info-panel">
           <h3>{campusInfo.name}</h3>
           <div className="info-item">
-            {/* استخدام أيقونة react-icons */}
+            
             <FaMapMarkerAlt className="info-icon" />
             <div>
               <strong>Address</strong>
@@ -31,8 +29,9 @@ const MapEmbed = () => {
             </div>
           </div>
           <div className="info-item">
-            {/* استخدام أيقونة react-icons */}
-            <FaCalendarAlt className="info-icon" />
+
+
+             <FaCalendarAlt className="info-icon" />
             <div>
               <strong>Office Hours</strong>
               <ul>
@@ -43,8 +42,7 @@ const MapEmbed = () => {
             </div>
           </div>
           <div className="map-link">
-            {/* استخدام أيقونة react-icons */}
-            <a href='https://maps.app.goo.gl/cbSxNemCKMksAG3h7'>
+             <a href={`https://www.google.com/maps?q=${encodeURIComponent(campusInfo.address)}`} target="_blank" rel="noopener noreferrer">
               <FaExternalLinkAlt className="link-icon" />
               Open in Google Maps
             </a>
@@ -68,5 +66,3 @@ const MapEmbed = () => {
 };
 
 export default MapEmbed;
-  
- 
